859e1cd8bf54f3e149c7dac7f56390601b35e054
anangfirmansyah <anangfirmansyahp5@gmail.com>
2023-08-16 13:55:32 +0700

    add form component

--- components/modals/StoreModal.tsx
+++ components/modals/StoreModal.tsx
@@ -2,9 +2,32 @@
 
 import { useStoreModal } from '@/hooks/useStoreModal';
 import Modal from '../ui/modal';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '../ui/button';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
+import { Input } from '../ui/input';
+
+const formSchema = z.object({
+	name: z.string().min(1),
+});
 
 export const StoreModal = () => {
 	const { isOpen, onClose, onOpen } = useStoreModal();
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			name: '',
+		},
+	});
+
+	const onSubmit = async (values: z.infer<typeof formSchema>) => {
+		// TODO Create store
+
+		console.log(values);
+	};
 
 	return (
 		<Modal
@@ -13,7 +36,39 @@ export const StoreModal = () => {
 			isOpen={isOpen}
 			onClose={onClose}
 		>
-			Future Create Store Form
+			<div>
+				<div className='space-y-4 py-2 pb-4'>
+					<Form {...form}>
+						<form onSubmit={form.handleSubmit(onSubmit)}>
+							<FormField
+								control={form.control}
+								name='name'
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Name</FormLabel>
+										<FormControl>
+											<Input
+												placeholder='E-Commerce'
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
+							<div className='pt-6 space-x-2 flex items-center justify-end w-full'>
+								<Button
+									variant={'outline'}
+									onClick={onClose}
+								>
+									Cancel
+								</Button>
+								<Button type='submit'>Continue</Button>
+							</div>
+						</form>
+					</Form>
+				</div>
+			</div>
 		</Modal>
 	);
 };
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -6,11 +6,11 @@ import { useEffect, useState } from 'react';
 export const ModalProvider = () => {
 	const [isMounted, setIsMounted] = useState(false);
 
-	useEffect(() => {
-		setIsMounted(true);
-	}, []);
+	// useEffect(() => {
+	// 	setIsMounted(true);
+	// }, []);
 
-	if (!isMounted) return null;
+	// if (!isMounted) return null;
 
 	return (
 		<>
